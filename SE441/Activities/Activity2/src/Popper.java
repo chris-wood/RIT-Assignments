6dc4531b3acddafaf3509613bc0f4ccf2c381f0a
Christopher Wood <caw4567@gmail.com>
2011-12-12 22:10:09 -0500

    Updated the Activity2 files for 441.

--- SE441/Activities/Activity2/src/Popper.java
+++ SE441/Activities/Activity2/src/Popper.java
@@ -1,19 +1,44 @@
-
+/**
+ * The Popper class that serves as an active
+ * thread trying to continuously pop strings
+ * from the top of a stack shared between 
+ * other Popper and Pusher objects.
+ * 
+ * @author Christopher Wood, caw4567@rit.edu
+ */
 public class Popper extends Thread 
 {
-	private final IStack stack;
+	/**
+	 * The shared stack object.
+	 */
+	final private IStack stack;
 	
+	/**
+	 * Constructor that initializes the parent Thread object
+	 * and stores the reference to the shared stack object.
+	 * 
+	 * @param name - name of this Thread object
+	 * @param stack - reference to the shared stack.
+	 */
 	public Popper(String name, final IStack stack)
 	{
 		super(name);
 		this.stack = stack;
 	}
 	
+	/**
+	 * Run method that continuously tries to pop elements
+	 * from the top of the stack. This method enters an infinite
+	 * loop and does not return until the program is terminated.
+	 */
+	@Override
 	public void run()
 	{
+		// Run forever until program terminated.
 		while (true)
 		{
-			String top = stack.pop();
+			// Pop and element and then give up the CPU.
+			stack.pop();
 			Thread.yield();
 		}
 	}
