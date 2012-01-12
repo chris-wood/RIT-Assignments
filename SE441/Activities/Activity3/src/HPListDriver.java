29819c11ba5855318ac6c7fedac7243036436361
Christopher Wood <caw4567@gmail.com>
2012-01-12 12:12:57 -0500

    Updated the driver to stub out the logic

--- SE441/Activities/Activity3/src/HPListDriver.java
+++ SE441/Activities/Activity3/src/HPListDriver.java
@@ -1,39 +1,53 @@
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class HPListDriver 
 {
 	class Client extends Thread
 	{
 		private HPList list;
 		private int count;
-		public Client(HPList list, int count)
+		private int id; 
+		
+		public Client(int id, HPList list, int count)
 		{
 			this.list = list;
+			this.id = id;
+			this.count = count;
 		}
 		
 		public void run()
 		{
 			for (int i = 0; i < count; i++)
 			{
-				
+				// TODO: insert and remove from the list right here
 			}
 		}
 	}
 	
 	private List<Client> clientList = new ArrayList<Client>();
 	private HPList list;
+	private int numClients;
 	
 	public HPListDriver(int numClients)
 	{
 		// create HPLlist
 		list = new HPList();
+		this.numClients = numClients;
 		
-		// create clients with access to list
-		Client c1 = new Client(list, NUM_ENTRIES);
-		Client c2 = new Client(list, NUM_ENTRIES);
-		Client c3 = new Client(list, NUM_ENTRIES);
+		for (int i = 0; i < numClients; i++)
+		{
+			clientList.add(new Client(i, list, NUM_ENTRIES));
+		}
+	}
+	
+	public void runTest()
+	{
+		for (int i = 0; i < numClients; i++)
+		{
+			System.out.println("starting!");
+			clientList.get(i).start();
+		}
 	}
 	
 	private static final int NUM_ENTRIES = 20;
@@ -41,7 +55,6 @@ public class HPListDriver
 	public static void main(String[] args)
 	{
 		HPListDriver driver = new HPListDriver(5);
-		
-		// throw stuff in list and display results
+		driver.runTest();
 	}
 }
