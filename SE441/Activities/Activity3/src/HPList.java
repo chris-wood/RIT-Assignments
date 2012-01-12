9b912d5a8eb4aadcc2cd60a6ba2f2703c0290c1d
Christopher Wood <caw4567@gmail.com>
2012-01-12 12:01:49 -0500

    Updated gitignore and other two files.

--- SE441/Activities/Activity3/src/HPList.java
+++ SE441/Activities/Activity3/src/HPList.java
@@ -5,8 +5,10 @@ import java.util.concurrent.locks.*;
  * comprises Nodes, and as few nodes as possible are locked by any thread so as
  * to permit the greatest possible concurrency.
  */
-public class HPList {
-	class Node {
+public class HPList 
+{
+	class Node 
+	{
 		final String value; // String in this node; a null string ("") is a
 							// dummy node.
 		Node next; // The node following this one.
@@ -14,7 +16,8 @@ public class HPList {
 		final Condition nextChanged; // Signaled when the next link for this
 										// node changes.
 
-		Node(String value, Node next) {
+		Node(String value, Node next) 
+		{
 			this.value = value;
 			this.next = next;
 			this.lock = new ReentrantLock();
@@ -28,7 +31,8 @@ public class HPList {
 	 * An HP list always contains at least two nodes - a dummy head and a dummy
 	 * tail.
 	 */
-	public HPList() {
+	public HPList() 
+	{
 		head = new Node("", new Node("", null));
 	}
 
@@ -37,32 +41,54 @@ public class HPList {
 	 * list, otherwise inserts it so as to keep the list in ascending order of
 	 * strings.
 	 */
-	public void insert(String s) {
+	public void insert(String s)
+	{
 		Node cur = head;
 		cur.lock.lock();
 		cur.next.lock.lock();
-		if (cur.next.value == "") {
-			Node newNode = new Node(s, cur.next);
-			cur.next = newNode;
-		} else {
-			while (cur.next.value != "") {
-				if (s.compareTo(cur.next.value) < 0) {
-					Node newNode = new Node(s, cur.next);
-					cur.next = newNode;
-					cur.lock.unlock();
-					cur.nextChanged.signalAll();
-					cur.next.lock.unlock();
-					return;
-				} else if (s.compareTo(cur.next.value) == 0) {
-					cur.lock.unlock();
-					cur.next.lock.unlock();
-					return;
-				} else {
-					cur.lock.unlock();
-					cur = cur.next;
-					cur.next.lock.lock();
+		try
+		{
+			if (cur.next.value == "") 
+			{
+				Node newNode = new Node(s, cur.next);
+				cur.next = newNode;
+			}
+			else
+			{
+				while (cur.next.value != "") 
+				{
+					if (s.compareTo(cur.next.value) < 0) 
+					{
+						Node newNode = new Node(s, cur.next);
+						cur.next = newNode;
+						cur.lock.unlock();
+						cur.nextChanged.signalAll();
+						cur.next.lock.unlock();
+						return;
+					} 
+					else if (s.compareTo(cur.next.value) == 0) 
+					{
+						cur.lock.unlock();
+						cur.next.lock.unlock();
+						return;
+					}
+					else 
+					{
+						cur.lock.unlock();
+						cur = cur.next;
+						cur.next.lock.lock();
+					}
 				}
 			}
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			cur.lock.unlock();
+			cur.next.lock.unlock();
 		}
 	}
 
@@ -75,37 +101,70 @@ public class HPList {
 	{
 		boolean found = false;
 		Node cur = head;
+		
+		// Acquire locks to the first two elements in the list in order
+		// to begin the list traversal
 		cur.lock.lock();
 		cur.next.lock.lock();
-		while(cur.next.value.compareTo(s) < 0) {
-			cur.lock.unlock();
-			cur = cur.next;
-			cur.next.lock.lock();
-		}
-
-		if (cur.next.value.equals("") && !block) {
-			return false;
-		} else if (cur.next.value.equals(s)) {
-			return true;
-		} else if (block) {
-			while(cur.next.value.compareTo(s) != 0) {
-				if(cur.next.value.compareTo(s) < 0) {
-					cur.lock.unlock();
-					cur = cur.next;
-					cur.next.lock.lock();
-				} else {
-					cur.lock.unlock();
-					cur.next.lock.unlock();
-					try {
-						cur.nextChanged.await();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					cur.lock.lock();
-					cur.next.lock.lock();
-				}
+		try
+		{
+			// Traverse the list until we find the element we are looking for
+			// or reach the spot where it should be located
+			while(cur.next.value.compareTo(s) < 0) 
+			{
+				cur.lock.unlock();
+				cur = cur.next;
+				cur.next.lock.lock();
 			}
-			return true;
+	
+			if (cur.next.value.equals("") && !block) 
+			{
+				return false;
+			} 
+			else if (cur.next.value.equals(s)) 
+			{
+				return true;
+			} 
+			else if (block) 
+			{
+				while(cur.next.value.compareTo(s) != 0) 
+				{
+					if(cur.next.value.compareTo(s) < 0) 
+					{
+						cur.lock.unlock();
+						cur = cur.next;
+						cur.next.lock.lock();
+					}
+					else
+					{
+						// Release the locks and await on a change signal
+						cur.lock.unlock();
+						cur.next.lock.unlock();
+						try 
+						{
+							cur.nextChanged.await();
+						}
+						catch (InterruptedException e) 
+						{
+							e.printStackTrace();
+						}
+						
+						// Re-acquire the lock to perform another check
+						cur.lock.lock();
+						cur.next.lock.lock();
+					}
+				}
+				return true;
+			}
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			cur.lock.unlock();
+			cur.next.lock.unlock();
 		}
 		
 		return found;
--- SE441/Activities/Activity3/src/HPListDriver.java
+++ SE441/Activities/Activity3/src/HPListDriver.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class HPListDriver 
 {
@@ -19,10 +22,26 @@ public class HPListDriver
 		}
 	}
 	
-	public static void main(String[] args)
+	private List<Client> clientList = new ArrayList<Client>();
+	private HPList list;
+	
+	public HPListDriver(int numClients)
 	{
 		// create HPLlist
+		list = new HPList();
+		
 		// create clients with access to list
+		Client c1 = new Client(list, NUM_ENTRIES);
+		Client c2 = new Client(list, NUM_ENTRIES);
+		Client c3 = new Client(list, NUM_ENTRIES);
+	}
+	
+	private static final int NUM_ENTRIES = 20;
+	
+	public static void main(String[] args)
+	{
+		HPListDriver driver = new HPListDriver(5);
+		
 		// throw stuff in list and display results
 	}
 }
