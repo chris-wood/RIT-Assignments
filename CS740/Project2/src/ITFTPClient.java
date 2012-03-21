26bdbba78489f73bc5c2e717f35fecbcd6bc480f
Christopher Wood <caw4567@gmail.com>
2012-03-20 21:42:36 -0400

    Started doing some schtuff for project 2!

---
+++ CS740/Project2/src/ITFTPClient.java
@@ -0,0 +1,15 @@
+/*
+ * ITFTPClient.java
+ * 
+ * Version: 3/20/12
+ */
+
+/**
+ * TODO
+ * 
+ * @author Christopher Wood (caw4567@rit.edu)
+ */
+public interface ITFTPClient 
+{
+	// TODO - define the appropriate methods
+}
