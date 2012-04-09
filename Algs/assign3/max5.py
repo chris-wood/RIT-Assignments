caa132ae34936f85fbff6cb5bd0f8fee6c2b2b42
Christopher Wood <caw4567@gmail.com>
2012-04-08 23:52:49 -0400

    Done with max5

--- Algs/assign3/max5.py
+++ Algs/assign3/max5.py
@@ -20,18 +20,35 @@ import sys
 FIXED_POWER = 31
 
 def max5(x1, x2, x3, x4, x5):
-	max1 = ((x1 + x2) / 2) + abs((x1 - x2) / 2)
-	max2 = ((max1 + x3) / 2) + abs((max1 - x3) / 2)
-	max3 = ((max2 + x4) / 2) + abs((max2 - x4) / 2)
-	max4 = ((max3 + x5) / 2) + abs((max3 - x5) / 2)
+	""" Compute the maximum of five numbers without any comparisons.
+		
+		The implementation of this function is based on the following
+		relation:
+		
+		max(a,b) = ((a+b)/2) + abs((a-b)/2)
+		
+		Thus, the function simply compounds the maximum value across 
+		all of the inputs and returns the result without ever using 
+		a single comparison.
+	"""
+	max1 = (x1 + x2 + abs(x1 - x2)) / 2
+	max2 = (max1 + x3 + abs(max1 - x3)) / 2
+	max3 = (max2 + x4 + abs(max2 - x4)) / 2
+	max4 = (max3 + x5 + abs(max3 - x5)) / 2
 	return max4
 
-""" Run the fibPow function.
+""" Run the max5 function.
 	
-	If the user enters a valid input, run it on that
-	number, else run it with a pre-defined constant.
-	"""
-if (len(sys.argv) > 1):
-	print fibPow(int(sys.argv[1]), 2, 1)[0]
+	If the user enters a valid set of numbers, run it on that
+	set, else run the program with some random numbers.
+"""
+if (len(sys.argv) == 6):
+	x1 = int(sys.argv[1])
+	x2 = int(sys.argv[2])
+	x3 = int(sys.argv[3])
+	x4 = int(sys.argv[4])
+	x5 = int(sys.argv[5])
+	print max5(x1, x2, x3, x4, x5)
 else:
-	print fibPow(FIXED_POWER, 2, 1)[0]
+	# Run the max5 routine with some random data
+	print max5(-102, 203.54321, -238, 555, 555.001)
