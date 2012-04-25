a36ffde969e5b6064dfc22c9847ef18edd06f48e
Christopher Wood <caw4567@gmail.com>
2012-04-25 00:33:22 -0400

    Updated algorithms homework

---
+++ OptMethods/project/code/bn.m
@@ -0,0 +1,9 @@
+function n = bn(S)
+	min = 0;
+	for i = 1:size(S)
+		%TODO: calculate elements to do weights
+		%TODO: calculate min value
+		%TODO: check to see if new minimum
+	end	
+	n = min;
+end
---
+++ OptMethods/project/code/wt.m
@@ -0,0 +1,6 @@
+function w = wt(x, n)
+	w = 0;
+	for i = 1:n
+		w = w + bitget(x, i);	
+	end
+end
