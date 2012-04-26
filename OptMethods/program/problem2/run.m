939a7516804ff9d5f08f594fe7cc8bba2253d991
Christopher Wood <caw4567@gmail.com>
2012-04-25 23:00:04 -0400

    Done with code comments.

--- OptMethods/program/problem2/run.m
+++ OptMethods/program/problem2/run.m
@@ -1,8 +1,16 @@
+% Description: This is a simple script that will initilize the fmincon
+%   solver to use the provided objective function and its constraints
+%   to produce an optimal solution using the interior-point algorithm.
+%
+% Author: Christopher Wood, caw4567@rit.edu
+% Date: 4/24/12
+% Version: 1.0
+
 % The initial value for the objective function.
 x0 = [0.2, 0.04];
 
-% Set up the options for the solver to make sure
-% the interior-point algorithm is used.
+% Set up the options for the solver to make sure the interior-point 
+% algorithm is used.
 options = optimset('Algorithm','interior-point','Display','iter','PlotFcns','optimplotfval');
 
 % Invoke the fmincon function to find the minimum.
