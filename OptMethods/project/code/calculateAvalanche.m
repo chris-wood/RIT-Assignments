c36adc812f5fc6bdd06c1e6c126ac43e7b6c27ec
Christopher Wood <caw4567@gmail.com>
2012-05-01 11:40:26 -0400

    Changed some of the objective function calculation code.

--- OptMethods/project/code/calculateAvalanche.m
+++ OptMethods/project/code/calculateAvalanche.m
@@ -16,10 +16,17 @@ indices = zeros(1, SBOX_SIZE);
 
 % Dumbly fill in the S-box contents
 for i = 1:SBOX_SIZE
-	S(i) = i - 1;
+	S(i) = 0; %i - 1
     indices(i) = i;
 end
 
 % Attempt to calculate the BN for this S-box (simple setup)
 S
 avalanche(S, SBOX_SIZE)
+
+% Set up the options for the solver to make sure the interior-point 
+% algorithm is used.
+options = optimset('Algorithm','interior-point','Display','iter-detailed','PlotFcns','optimplotfval');
+
+% Invoke the fmincon function to find the minimum.
+[v1,v2] = fmincon('sboxavalancheobj',S,[],[],[],[],0,(2^SBOX_SIZE) - 1,'sboxcon',options);
