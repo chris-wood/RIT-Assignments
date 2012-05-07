7866f8fa5406b7fccc150fb1c2dacc75c3e0a3cb
Christopher Wood <caw4567@gmail.com>
2012-05-07 17:36:49 -0400

    Tried some changes for multi objective optimization with GA, didn't work…

--- OptMethods/project/code/Joint/order4/joint.m
+++ OptMethods/project/code/Joint/order4/joint.m
@@ -4,12 +4,12 @@ function y = joint( X )
 
 % Initialize for two objectives 
 %y = zeros(2,1);
-y = zeros(1,1);
+%y = zeros(1,1);
 
 % Compute objectives
-disp(X);
-y(1) = avalanche_fitness(X);
-%y(2) = bn_fitness(X);
+%disp(X);
+y(1) = avalanche_fitness(X)
+y(2) = bn_fitness(X)
 
 
 end
