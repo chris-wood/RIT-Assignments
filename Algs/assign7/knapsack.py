b35c48eee78424a13e592e040e00316f5abf461f
Christopher Wood <caw4567@gmail.com>
2012-05-12 12:29:42 -0400

    DP algorithm is working.

--- Algs/assign7/knapsack.py
+++ Algs/assign7/knapsack.py
@@ -22,16 +22,28 @@ def knapsack(n, v, w, W):
 	"""
 	# Construct the DP table and item index list
 	indexList = list()
-	table = list()
-	for i in range(0, n):
+	value = list() #value is table of [item][capacity]
+	for i in range(0, n + 1):
 		rowList = list()
-		for j in range(0, n):
+		for j in range(0, W + 1):
 			rowList.append(0)
-		table.append(rowList)
+		value.append(rowList)
 		indexList.append(0)
 		
-	# Do the DP part now, kthx
+	# Do the DP part now
+	for i in range(0, n + 1):
+		for j in range(0, W + 1):
+			if (i == 0 or j == 0):
+				# Both base cases handled this way
+				value[i][j] = 0
+			else:
+				# Check to see if we gain more value by including this element
+				# or not
+				value[i][j] = max(v[i - 1] + value[i - 1][j - w[i - 1]], 
+								  value[i - 1][j])
 
+	print value[n][W]
+			
 """ Run the knapsack function.
 	
 	If the user enters a valid set of numbers, run it on that
@@ -41,6 +53,5 @@ if (len(sys.argv) > 2):
 	print "TODO"
 
 else:
-	print "TODO"
-	knapsack(5, [], [], 5)
+	knapsack(3, [60,100,120], [10,20,30], 50)
 
