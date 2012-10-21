dcae50e84d717bc6869bce03898441eebaa825b7
Christopher Wood <christopherwood07@gmail.com>
2012-10-20 22:37:06 -0400

    Added some content to the xarray Ruby file.

--- 20121/PLC/Project4/xarray.rb
+++ 20121/PLC/Project4/xarray.rb
@@ -28,31 +28,48 @@ module XArray
   end
 
   # DEFINE [] HERE
-  def [] index
-    return @a[index] # TODO: this is not correct...
+  def [] (start, len = nil)
+    if (start < 0)
+      start = start + @a.length
+    end
+    return nil if (start < 0 || start >= @a.length)
+
+    # Check to see what we should return
+    if (len == nil)
+      return @a[start]
+    else
+      # Make sure we don't run past the upper bound
+      return nil if (start + len >= @a.length)
+
+      # Build the resulting array
+      result = []
+      for i in start..(start + len)
+        result << @a[i]
+      end
+      return result
+    end
   end
 
   # DEFINE each HERE
-  def each &block
-    @a.each {|c| c.each &block}
+  def each #&block #caw
+    @a.each {|c| yield c} #c.each &block #caw
+    self
   end
 
   # DEFINE <=> HERE
-  def <=> otherArray
+  def <=> (othr)
     # Fetch the array lengths
     length1 = @a.length - 1
-    length2 = otherArray.length - 1
+    length2 = othr.length - 1
 
+    maxVal = [length1, length2].max
     val = 0
-    for i in 0..length1
-      if (i < length2)
-        if (@a[i] != otherArray[i])
-          return @a[i] <=> otherArray[i]  #TODO: not working correctly
-        end
-      else
-        return val
+    for i in 0..maxVal
+      if (@a[i] != othr[i])
+        return @a[i] <=> othr[i]  #TODO: not working correctly
       end
     end
+    return val
   end
 
 end
@@ -82,12 +99,26 @@ class ConcatXArray
   include XArray
 
   # DEFINE initialize HERE
+  def initialize(a1, a2)
+    @a = a1.clone
+    a2.each {|x| @a << x}
+  end
 
   # DEFINE inspect HERE
+  def inspect
+    "ConcatXArray(#{@a.inspect})"
+  end
 
   # DEFINE length HERE
+  def length
+    @a.length
+  end
 
   # DEFINE elem_at HERE
+  def elem_at i
+    return nil if (i < 0 || i >= length)
+    @a[i]
+  end
 
 end
 
@@ -95,12 +126,28 @@ class RepeatXArray
   include XArray
 
   # DEFINE initialize HERE
+  def initialize(a, n)
+    @a = []
+    for i in 0..n
+      a.each {|x| @a << x}
+    end
+  end
 
   # DEFINE inspect HERE
+  def inspect
+    "RepeatXArray(#{@a.inspect})"
+  end
 
   # DEFINE length HERE
+  def length
+    @a.length
+  end
 
   # DEFINE elem_at HERE
+  def elem_at i
+    return nil if (i < 0 || i >= length)
+    @a[i]
+  end
 
 end
 
@@ -108,12 +155,26 @@ class ReverseXArray
   include XArray
 
   # DEFINE initialize HERE
+  def initialize(a)
+    @a = []
+    a.each {|x| @a.insert(0, x)}
+  end
 
   # DEFINE inspect HERE
+  def inspect
+    "ReverseXArray(#{@a.inspect})"
+  end
 
   # DEFINE length HERE
+  def length
+    @a.length
+  end
 
   # DEFINE elem_at HERE
+  def elem_at i
+    return nil if (i < 0 || i >= length)
+    @a[i]
+  end
 
 end
 
@@ -121,12 +182,27 @@ class PalindromeXArray
   include XArray
 
   # DEFINE initialize HERE
+  def initialize(a)
+    @a = a.clone()
+    index = a.length
+    a.each {|x| @a.insert(index, x)}
+  end
 
   # DEFINE inspect HERE
+  def inspect
+    "PalindromeXArray(#{@a.inspect})"
+  end
 
   # DEFINE length HERE
+  def length
+    @a.length
+  end
 
   # DEFINE elem_at HERE
+  def elem_at i
+    return nil if (i < 0 || i >= length)
+    @a[i]
+  end
 
 end
 
@@ -134,12 +210,37 @@ class SwizzleXArray
   include XArray
 
   # DEFINE initialize HERE
+  def initialize(a1, a2)
+    @a = []
+    a1length = a1.length
+    a2length = a2.length
+    max = [a1length, a2length].max
+
+    for i in 0..max
+      if (i < a1length)
+        @a << a1[i]
+      end
+      if (i < a2length)
+        @a << a2[i]
+      end
+    end
+  end
 
   # DEFINE inspect HERE
+  def inspect
+    "SwizzleXArray(#{@a.inspect})"
+  end
 
   # DEFINE length HERE
+  def length
+    @a.length
+  end
 
   # DEFINE elem_at HERE
+  def elem_at i
+    return nil if (i < 0 || i >= length)
+    @a[i]
+  end
 
 end
 
@@ -147,11 +248,27 @@ class ProcXArray
   include XArray
 
   # DEFINE initialize HERE
+  def initialize(n, p)
+    @a = []
+    for i in 0..n
+      @a << (p.call i)
+    end
+  end
 
   # DEFINE inspect HERE
+  def inspect
+    "ProcXArray(#{@a.inspect})"
+  end
 
   # DEFINE length HERE
+  def length
+    @a.length
+  end
 
   # DEFINE elem_at HERE
+  def elem_at i
+    return nil if (i < 0 || i >= length)
+    @a[i]
+  end
 
 end
