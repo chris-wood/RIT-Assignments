82b43301610d005be29293e5869fd6cb15b6e6e7
Christopher Wood <christopherwood07@gmail.com>
2012-12-05 00:14:57 -0500

    Support for all hashlib routines, able to crack those in the dictionary. Need to add support for salted passwords?

--- 20122/SMT/Assignments/Assignment2/part3/wedge.py
+++ 20122/SMT/Assignments/Assignment2/part3/wedge.py
@@ -9,7 +9,7 @@
 # Used libraries
 import hashlib # ('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512')
 import crypt # crypt(3)
-import sys
+import sys # for command-line arguments and file I/O 
 
 #easyRange = [32,33,36,42,43] + range(48, 58) + range(65, 91) + range(97, 123)
 #allRange = range(32,127)
@@ -88,6 +88,7 @@ def parse_commandline_string(param):
 def read_password():
     ''' TODO
     '''
+    print("")
     password = raw_input("Enter a password: ")
     return password
 
@@ -120,29 +121,40 @@ def generateHash(password, format):
     else:
         raise Exception("Invalid hash format")
 
+    return digest
+
 def compare_password_hashes(word, password, format):
     ''' TODO
     '''
+
+    match = False
+
     if (format == "crypt"):
         pass
     elif (format == "md5"):
         h = hashlib.md5()
         h.update(word)
-        if ()
+        if (h.digest() == password):
+            match = True
     elif (format == "sha1"):
         h = hashlib.sha1()
-        h.update(password)
-        digest = h.digest()
+        h.update(word)
+        if (h.digest() == password):
+            match = True
     elif (format == "sha256"):
         h = hashlib.sha256()
-        h.update(password)
-        digest = h.digest()
+        h.update(word)
+        if (h.digest() == password):
+            match = True
     elif (format == "sha512"):
         h = hashlib.sha512()
-        h.update(password)
-        digest = h.digest()
+        h.update(word)
+        if (h.digest() == password):
+            match = True
     else:
         raise Exception("Invalid hash format")
+
+    return match
 
 def crack_password(format, password):
     ''' TODO
@@ -150,14 +162,19 @@ def crack_password(format, password):
     ('md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512')
     '''
     global DICTIONARY
+    cracked = False
     try:
         with open(DICTIONARY) as f: 
             for word in f.readlines():
 
                 # TODO: candidate mangling goes here
 
-                if (compare_password_hashes(word, password, format))
-                    print("Password found: " + word)
+                if (compare_password_hashes(word.rstrip('\n'), password, format)):
+                    print("Password found: " + word.rstrip('\n'))
+                    cracked = True
+                    return cracked
+        if (cracked == False):
+            print("Password crack was unsuccessful.")
                 
     except:
         raise Exception("Error occurred while cracking password")
@@ -190,11 +207,11 @@ def main():
             raise Exception("Invalid number of command-line parameters")
 
         # Display the cracking options
+        print_params()
         try:
             if (len(hashFile) > 0):
                 with open(hashFile) as f: 
-                    print_params()
-
+                    pass
                     # TODO: read from the file and then execute the crack_password for each line
             else:
                 password = read_password()
