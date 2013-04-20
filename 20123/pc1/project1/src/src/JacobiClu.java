a50d29f3248c21eb7196fa2872e703673b246b4c
Christopher Wood <christopherwood07@gmail.com>
2013-04-20 15:17:17 -0400

    Odd cases are all fucked up, but the gist is there and working correctly for the smaller outputs.

--- 20123/pc1/project1/src/src/JacobiClu.java
+++ 20123/pc1/project1/src/src/JacobiClu.java
@@ -99,7 +99,7 @@ public class JacobiClu
 		try
 		{
 			// Parse the command line arguments.
-			int first, last;
+//			int first, last;
 			final int n = Integer.parseInt(args[0]);
 			if (n < 1)
 			{
@@ -108,106 +108,16 @@ public class JacobiClu
 			}
 			final long seed = Long.parseLong(args[1]);
 			
-			// Set up the ranges for each process.
-			Range[] ranges = new Range(0, n - 1).subranges(size);
-			Range myrange = ranges[rank];
-			first = myrange.lb();
-			last = myrange.ub() + 1;
-			
-			// Only allocate the required space for A, x, y, and b in 
-			// each process.
-			x = new double[n];
-			b = new double[last - first];
-			A = new double[last - first][];
-			y = new double[last - first];
-			
-			double[] A_i;
-
-			Random prng_thread = Random.getInstance(seed);
-			prng_thread.setSeed(seed);
-			prng_thread.skip((n + 1) * first);
-			for (int i = first; i < last; i++)
+			try 
 			{
-				A[i - first] = new double[n];
-				A_i = A[i - first]; 
-				for (int j = 0; j < n; j++)
-				{
-//	        		System.out.println(rank + " trying " + first + " - " + last + " for A matrix index with i = " + i);
-					A_i[j] = (prng_thread.nextDouble() * 9.0) + 1.0;
-				}
-				A_i[i] += 10.0 * n;
-				b[i - first] = (prng_thread.nextDouble() * 9.0) + 1.0;
-				y[i - first] = 1.0;
+				JacobiClu solver = new JacobiClu();
+				solver.initAndSolve(n, seed);
+			} 
+			catch (Exception e) 
+			{
+				System.err.println("An error occurred while solving the system.");
+				e.printStackTrace();
 			}
-    		
-    		// Allocated buffers for communication and whatnot
-    		boolean converged = false;
-    		xBuffers = DoubleBuf.sliceBuffers(x, ranges);
-    		yBuffer = DoubleBuf.buffer(y);
-    		double xVal;
-			double yVal;
-			double sum;
-			int offset;
-			boolean p_iterSuccess;
-			
-			long end1 = System.currentTimeMillis();
-			System.out.println("time = " + (end1 - startTime));
-			
-    		while (!converged) 
-    		{
-    			// Every process needs the -entire- x vector, so we need 
-    			// to do an "all gather"
-    			world.allGather(yBuffer, xBuffers); // good
-    			p_iterSuccess = true;
-    			long commEnd = System.currentTimeMillis();
-//    			int offset;
-//    			System.out.println((n-1) + " " + first + " " + last);
-    			for (int i = first; i < last; i++) // first - last ---- ACCESS TO LAST IS ADDING 100MS
-    			{
-    				// Compute the upper and lower matrix product, omitting
-    				// the element at index i
-    				offset = i - first;
-    				A_i = A[offset];
-//    				A_i = A[i];
-    				yVal = sum = 0.0;
-    				xVal = x[i];
-    				for (int index = 0; index < i; index++)
-    				{
-    					sum += (A_i[index] * x[index]);
-    				}
-    				for (int index = i + 1; index < n; index++)
-    				{
-    					sum += (A_i[index] * x[index]);
-    				}
-    				
-    				// Compute the y[] value.
-    				yVal = (b[i] - sum) / A_i[i]; // b[offset]
-
-    				// Check for convergence.
-    				if (!(Math.abs((2 * (xVal - yVal)) 
-    							/ (xVal + yVal)) < epsilon))
-    				{
-    					p_iterSuccess = false;
-    				}
-    				
-    				// Store the y coordinate value.
-//    				y[offset] = yVal;
-    				y[i] = yVal;
-    			}
-    			long compEnd = System.currentTimeMillis();
-				
-				// Make everyone report their convergence results
-				BooleanBuf bBuf = BooleanBuf.buffer(p_iterSuccess);
-				world.allReduce(bBuf, BooleanOp.AND); // ARK does this.
-				converged = bBuf.get(0);
-
-//				long end = System.currentTimeMillis();
-//				System.out.println("---");
-//				System.out.println(commEnd - start);
-				System.out.println(compEnd - commEnd);
-//				System.out.println(end - compEnd);
-//				System.out.println(end - start);
-    		}
 			
 			// Display the solution and time (from the root process)
 			if (rank == 0)
@@ -247,6 +157,94 @@ public class JacobiClu
 		{
 			error("Error in the ParallelRegion run() method.");
 			ex1.printStackTrace();
+		}
+	}
+	
+	public void initAndSolve(int n, long seed) throws IOException 
+	{
+		// Set up the ranges for each process.
+		Range[] ranges = new Range(0, n - 1).subranges(size);
+		Range myrange = ranges[rank];
+		int first = myrange.lb();
+		int last = myrange.ub() + 1;
+		
+		// Only allocate the required space for A, x, y, and b in 
+		// each process.
+		x = new double[n];
+		b = new double[last - first];
+		A = new double[last - first][];
+		y = new double[last - first];
+		double[] A_i; // temporary row storage 
+
+		Random prng_thread = Random.getInstance(seed);
+		prng_thread.setSeed(seed);
+		prng_thread.skip((n + 1) * first);
+		for (int i = first; i < last; i++)
+		{
+			A[i - first] = new double[n];
+			A_i = A[i - first]; 
+			for (int j = 0; j < n; j++)
+			{
+				A_i[j] = (prng_thread.nextDouble() * 9.0) + 1.0;
+			}
+			A_i[i] += 10.0 * n;
+			b[i - first] = (prng_thread.nextDouble() * 9.0) + 1.0;
+			y[i - first] = 1.0;
+		}
+		
+		// Allocated buffers for communication and algorithm control
+		xBuffers = DoubleBuf.sliceBuffers(x, ranges);
+		yBuffer = DoubleBuf.buffer(y);
+		boolean converged = false;
+		boolean p_iterSuccess;
+		double xVal;
+		double yVal;
+		double sum;
+		int offset;
+		
+		// Initialize the x vector to all 1.0
+		for (int i = 0; i < n; i++) x[i] = 1.0;
+		
+		// Loop until everyone converges.
+		while (!converged) 
+		{
+			p_iterSuccess = true;
+			for (int i = first; i < last; i++) 
+			{
+				// Compute the upper and lower matrix product, omitting
+				// the element at index i.
+				offset = i - first;
+				A_i = A[offset];
+				yVal = sum = 0.0;
+				xVal = x[i];
+				for (int index = 0; index < i; index++)
+				{
+					sum += (A_i[index] * x[index]);
+				}
+				for (int index = i + 1; index < n; index++)
+				{
+					sum += (A_i[index] * x[index]);
+				}
+				
+				// Compute and store the y coordinate value.
+				yVal = (b[offset] - sum) / A_i[i]; 
+				y[offset] = yVal;
+
+				// Check for convergence.
+				if (p_iterSuccess && !(Math.abs((2 * (xVal - yVal)) 
+							/ (xVal + yVal)) < epsilon))
+				{
+					p_iterSuccess = false;
+				}
+			}
+			
+			// Swap the x/y vectors by distributing y partitions to each process
+			world.allGather(yBuffer, xBuffers); 
+			
+			// Make everyone report their convergence results
+			BooleanBuf bBuf = BooleanBuf.buffer(p_iterSuccess);
+			world.allReduce(bBuf, BooleanOp.AND); // ARK does this.
+			converged = bBuf.get(0);
 		}
 	}
 
